3286aae42edf84d5686e84721efb649115b79969
Giedre <giedre.rageliene@gmail.com>
2023-09-04 10:25:00 +0300

    Footer jsx

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export function Footer() {
+    return (
+        <div className="container">
+            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                <span className="mb-3 mb-md-0 align-items-cente text-body-secondary">© 2023 Padekime, Inc</span>
+            </footer>
+        </div>
+    );
+}
